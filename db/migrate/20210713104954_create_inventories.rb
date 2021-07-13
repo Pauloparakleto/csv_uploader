ef4301fd1f9ef72fe5198254262948a030e593f8
paulo-felipe <alparakleto@gmail.com>
2021-07-13 07:51:41 -0300

    feat(inventory.rb): generate model inventory.rb and factory bot

---
+++ db/migrate/20210713104954_create_inventories.rb
@@ -0,0 +1,11 @@
+class CreateInventories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :inventories do |t|
+      t.string :name
+      t.string :value
+      t.string :quantity
+
+      t.timestamps
+    end
+  end
+end
