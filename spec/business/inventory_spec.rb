5dc93227cabb9d3fdf36f155e869aadabd45d754
paulo-felipe <alparakleto@gmail.com>
2021-07-14 08:36:27 -0300

    test(inventory_spec.rb): add initial test

---
+++ spec/business/inventory_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+RSpec.describe "Inventory" do
+  context "Create" do
+    it 'valid attributes' do
+      inventory = FactoryBot.create(:inventory, name: "samsung", value: 10.0, quantity: 4)
+
+      expect(inventory.name).to eq("samsung")
+      expect(inventory.value).to eq(10.0)
+      expect(inventory.quantity).to eq(4)
+    end
+  end
+end
