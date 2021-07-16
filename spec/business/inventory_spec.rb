ffe888ccc13d74555f0881af8609e9bb6327adff
paulo-felipe <alparakleto@gmail.com>
2021-07-16 08:34:19 -0300

    refactor(inventory_spec.rb): best rspec-cop practices

--- spec/business/inventory_spec.rb
+++ spec/business/inventory_spec.rb
@@ -1,11 +1,21 @@
 require "rails_helper"
 RSpec.describe Inventory do
   context "when create" do
-    it "valid attributes" do
+    it "name" do
       inventory = FactoryBot.create(:inventory, name: "samsung", value: 10.0, quantity: 4)
 
       expect(inventory.name).to eq("samsung")
+    end
+
+    it "value" do
+      inventory = FactoryBot.create(:inventory, name: "samsung", value: 10.0, quantity: 4)
+
       expect(inventory.value).to eq(10.0)
+    end
+
+    it "quantity" do
+      inventory = FactoryBot.create(:inventory, name: "samsung", value: 10.0, quantity: 4)
+
       expect(inventory.quantity).to eq(4)
     end
   end
