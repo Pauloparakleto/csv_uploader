dcca48f98fe74ff6547bcf244fca78ac5f4b5ca9
Pauloparakleto <alparakleto@gmail.com>
2021-07-29 14:07:24 -0300

    chore(factories/inventories.rb): add details definitions

--- spec/factories/inventories.rb
+++ spec/factories/inventories.rb
@@ -3,5 +3,10 @@ FactoryBot.define do
     name { "MyString" }
     value { "MyString" }
     quantity { "MyString" }
+    manufacturer { "MyString" }
+    model { "MyString" }
+    color { "MyString" }
+    carrier_plan_type { "MyString" }
+    price { 999.98 }
   end
 end
