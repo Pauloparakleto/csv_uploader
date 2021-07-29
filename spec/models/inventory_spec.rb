db1e3d1cd513b019927da31f4e903df8a27e8d53
Pauloparakleto <alparakleto@gmail.com>
2021-07-29 14:10:19 -0300

    chore(inventory_spec.rb): rubocop autocorrect

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -1,44 +1,45 @@
 require "rails_helper"
 
 RSpec.describe Inventory, type: :model do
+  # TODO, check the difference between price decimal and value integer
   context "on create" do
-    it 'valid' do
+    it "valid" do
       inventory = FactoryBot.create(:inventory)
 
       expect(inventory).to be_valid
     end
 
-    it 'manufacturer' do
+    it "manufacturer" do
       inventory = FactoryBot.create(:inventory)
 
       expect(inventory.manufacturer).to be_truthy
     end
 
-    it 'model' do
+    it "model" do
       inventory = FactoryBot.create(:inventory)
 
       expect(inventory.model).to be_truthy
-      end
+    end
 
-    it 'color' do
+    it "color" do
       inventory = FactoryBot.create(:inventory)
 
       expect(inventory.color).to be_truthy
-      end
+    end
 
-    it 'carrier_plan_type' do
+    it "carrier_plan_type" do
       inventory = FactoryBot.create(:inventory)
 
       expect(inventory.carrier_plan_type).to be_truthy
-      end
+    end
 
-    it 'quantity' do
+    it "quantity" do
       inventory = FactoryBot.create(:inventory)
 
       expect(inventory.quantity).to be_truthy
-      end
+    end
 
-    it 'price' do
+    it "price" do
       inventory = FactoryBot.create(:inventory)
 
       expect(inventory.price).to be_truthy
