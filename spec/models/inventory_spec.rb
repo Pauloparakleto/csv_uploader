08bd7364f6582105cafe792dcdbcb7816cee19d2
Pauloparakleto <alparakleto@gmail.com>
2021-07-29 14:06:02 -0300

    test(models/inventory_spec.rb): add valid attributes context

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -1,5 +1,47 @@
 require "rails_helper"
 
 RSpec.describe Inventory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "on create" do
+    it 'valid' do
+      inventory = FactoryBot.create(:inventory)
+
+      expect(inventory).to be_valid
+    end
+
+    it 'manufacturer' do
+      inventory = FactoryBot.create(:inventory)
+
+      expect(inventory.manufacturer).to be_truthy
+    end
+
+    it 'model' do
+      inventory = FactoryBot.create(:inventory)
+
+      expect(inventory.model).to be_truthy
+      end
+
+    it 'color' do
+      inventory = FactoryBot.create(:inventory)
+
+      expect(inventory.color).to be_truthy
+      end
+
+    it 'carrier_plan_type' do
+      inventory = FactoryBot.create(:inventory)
+
+      expect(inventory.carrier_plan_type).to be_truthy
+      end
+
+    it 'quantity' do
+      inventory = FactoryBot.create(:inventory)
+
+      expect(inventory.quantity).to be_truthy
+      end
+
+    it 'price' do
+      inventory = FactoryBot.create(:inventory)
+
+      expect(inventory.price).to be_truthy
+    end
+  end
 end
