a66ac6e07f3e51c6dec8768cc44c7ef5d6798413
Pauloparakleto <alparakleto@gmail.com>
2021-07-30 20:16:16 -0300

    feat(model/inventory.rb): validates attributes with message

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,2 +1,10 @@
 class Inventory < ApplicationRecord
+  validates :name, presence: { message: "O nome não pode ficar em branco" }
+  validates :value, presence: { message: "O valor não pode ficar em branco" }
+  validates :quantity, presence: { message: "A quantidade não pode ficar em branco" }
+  validates :manufacturer, presence: { message: "O nome do fabricante não pode ficar em branco" }
+  validates :model, presence: { message: "O modelo não pode ficar em branco" }
+  validates :color, presence: { message: "A cor não pode ficar em branco" }
+  validates :carrier_plan_type, presence: { message: "O plano do aparelho não pode ficar em branco" }
+  validates :price, presence: { message: "O preço não pode ficar em branco" }
 end
