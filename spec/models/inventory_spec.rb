c7604b646254a7b3a15fd9423421af07a37a319d
Pauloparakleto <alparakleto@gmail.com>
2021-07-30 20:29:30 -0300

    refactor(inventory_spec.rb): extract repeated call to a single method

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -1,49 +1,39 @@
 require "rails_helper"
 
+def inventory_valid
+  FactoryBot.create(:inventory)
+end
+
 RSpec.describe Inventory, type: :model do
   # TODO, check the difference between price decimal and value integer
   describe "Valid" do
     context "when create" do
       it "valid" do
-        inventory = FactoryBot.create(:inventory)
-
-        expect(inventory).to be_valid
+        expect(inventory_valid).to be_valid
       end
 
       it "manufacturer" do
-        inventory = FactoryBot.create(:inventory)
-
-        expect(inventory.manufacturer).to be_truthy
+        expect(inventory_valid.manufacturer).to be_truthy
       end
 
       it "model" do
-        inventory = FactoryBot.create(:inventory)
-
-        expect(inventory.model).to be_truthy
+        expect(inventory_valid.model).to be_truthy
       end
 
       it "color" do
-        inventory = FactoryBot.create(:inventory)
-
-        expect(inventory.color).to be_truthy
+        expect(inventory_valid.color).to be_truthy
       end
 
       it "carrier_plan_type" do
-        inventory = FactoryBot.create(:inventory)
-
-        expect(inventory.carrier_plan_type).to be_truthy
+        expect(inventory_valid.carrier_plan_type).to be_truthy
       end
 
       it "quantity" do
-        inventory = FactoryBot.create(:inventory)
-
-        expect(inventory.quantity).to be_truthy
+        expect(inventory_valid.quantity).to be_truthy
       end
 
       it "price" do
-        inventory = FactoryBot.create(:inventory)
-
-        expect(inventory.price).to be_truthy
+        expect(inventory_valid.price).to be_truthy
       end
     end
   end
