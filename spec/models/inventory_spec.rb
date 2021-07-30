867524ec69de0e95783b8dd34cd3b347455cb8d9
Pauloparakleto <alparakleto@gmail.com>
2021-07-30 20:45:40 -0300

    refactor(inventory_spec.rb): extract invalid atributes to a single method

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -4,6 +4,12 @@ def inventory_valid
   FactoryBot.create(:inventory)
 end
 
+def invalid_inventory(attribute)
+  inventory = FactoryBot.build(:inventory, "#{attribute}": nil)
+  inventory.save
+  inventory
+end
+
 RSpec.describe Inventory, type: :model do
   # TODO, check the difference between price decimal and value integer
   describe "Valid" do
@@ -41,115 +47,67 @@ RSpec.describe Inventory, type: :model do
   describe "Invalid" do
     context "when create" do
       it "nil name" do
-        inventory = FactoryBot.build(:inventory, name: nil)
-        inventory.save
-
-        expect(inventory).not_to be_valid
+        expect(invalid_inventory("name")).not_to be_valid
       end
 
       it "nil name message" do
-        inventory = FactoryBot.build(:inventory, name: nil)
-        inventory.save
-
-        expect(inventory.errors.messages.first.second).to eq(["O nome não pode ficar em branco"])
+        expect(invalid_inventory("name").errors.messages.first.second).to eq(["O nome não pode ficar em branco"])
       end
 
       it "nil value" do
-        inventory = FactoryBot.build(:inventory, value: nil)
-        inventory.save
-
-        expect(inventory).not_to be_valid
+        expect(invalid_inventory("value")).not_to be_valid
       end
 
       it "nil value message" do
-        inventory = FactoryBot.build(:inventory, value: nil)
-        inventory.save
-
-        expect(inventory.errors.messages.first.second).to eq(["O valor não pode ficar em branco"])
+        expect(invalid_inventory("value").errors.messages.first.second).to eq(["O valor não pode ficar em branco"])
       end
 
       it "nil quantity" do
-        inventory = FactoryBot.build(:inventory, quantity: nil)
-        inventory.save
-
-        expect(inventory).not_to be_valid
+        expect(invalid_inventory("quantity")).not_to be_valid
       end
 
       it "nil quantity message" do
-        inventory = FactoryBot.build(:inventory, quantity: nil)
-        inventory.save
-
-        expect(inventory.errors.messages.first.second).to eq(["A quantidade não pode ficar em branco"])
+        expect(invalid_inventory("quantity").errors.messages.first.second).to eq(["A quantidade não pode ficar em branco"])
       end
 
       it "nil manufacturer" do
-        inventory = FactoryBot.build(:inventory, manufacturer: nil)
-        inventory.save
-
-        expect(inventory).not_to be_valid
+        expect(invalid_inventory("manufacturer")).not_to be_valid
       end
 
       it "nil manufacturer message" do
-        inventory = FactoryBot.build(:inventory, manufacturer: nil)
-        inventory.save
-
-        expect(inventory.errors.messages.first.second).to eq(["O nome do fabricante não pode ficar em branco"])
+        expect(invalid_inventory("manufacturer").errors.messages.first.second).to eq(["O nome do fabricante não pode ficar em branco"])
       end
 
       it "nil model" do
-        inventory = FactoryBot.build(:inventory, model: nil)
-        inventory.save
-
-        expect(inventory).not_to be_valid
+        expect(invalid_inventory("model")).not_to be_valid
       end
 
       it "nil model message" do
-        inventory = FactoryBot.build(:inventory, model: nil)
-        inventory.save
-
-        expect(inventory.errors.messages.first.second).to eq(["O modelo não pode ficar em branco"])
+        expect(invalid_inventory("model").errors.messages.first.second).to eq(["O modelo não pode ficar em branco"])
       end
 
       it "nil color" do
-        inventory = FactoryBot.build(:inventory, color: nil)
-        inventory.save
-
-        expect(inventory).not_to be_valid
+        expect(invalid_inventory("color")).not_to be_valid
       end
 
       it "nil color message" do
-        inventory = FactoryBot.build(:inventory, color: nil)
-        inventory.save
-
-        expect(inventory.errors.messages.first.second).to eq(["A cor não pode ficar em branco"])
+        expect(invalid_inventory("color").errors.messages.first.second).to eq(["A cor não pode ficar em branco"])
       end
 
       it "nil carrier_plan_type" do
-        inventory = FactoryBot.build(:inventory, carrier_plan_type: nil)
-        inventory.save
-
-        expect(inventory).not_to be_valid
+        expect(invalid_inventory("carrier_plan_type")).not_to be_valid
       end
 
       it "nil carrier_plan_type message" do
-        inventory = FactoryBot.build(:inventory, carrier_plan_type: nil)
-        inventory.save
-
-        expect(inventory.errors.messages.first.second).to eq(["O plano do aparelho não pode ficar em branco"])
+        expect(invalid_inventory("carrier_plan_type").errors.messages.first.second).to eq(["O plano do aparelho não pode ficar em branco"])
       end
 
       it "nil price" do
-        inventory = FactoryBot.build(:inventory, price: nil)
-        inventory.save
-
-        expect(inventory).not_to be_valid
+        expect(invalid_inventory("price")).not_to be_valid
       end
 
       it "nil price message" do
-        inventory = FactoryBot.build(:inventory, price: nil)
-        inventory.save
-
-        expect(inventory.errors.messages.first.second).to eq(["O preço não pode ficar em branco"])
+        expect(invalid_inventory("price").errors.messages.first.second).to eq(["O preço não pode ficar em branco"])
       end
     end
   end
