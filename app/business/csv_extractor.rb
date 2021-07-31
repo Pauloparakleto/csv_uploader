473ae76b34a06030648692951727abb6e536e95b
Pauloparakleto <alparakleto@gmail.com>
2021-07-31 14:25:29 -0300

    feat(csv_extractor.rb): add initial build method

--- app/business/csv_extractor.rb
+++ app/business/csv_extractor.rb
@@ -1,11 +1,36 @@
 class CsvExtractor
-  def initialize(array: nil)
-    @array = array
+  def initialize(path: nil)
+    @path = path
   end
 
   def valid?
+    data = CSV.read(@path)
     result = []
-    @array.each { |i| result << false if i.any? nil }
+    data.each { |i| result << false if i.any? nil }
     result.empty?
+  end
+
+  def build
+    data = CSV.read(@path)
+    array = []
+    data.each do
+      array << Inventory.create(attributes)
+    end
+    array
+  end
+
+  def attributes
+    data = CSV.read(@path)
+    {
+      name: "MyString",
+      value: "MyString",
+      quantity: "MyString",
+      manufacturer: "MyString",
+      model: "MyString",
+      color: "MyString",
+      carrier_plan_type: "MyString",
+      price: 999.98,
+    }
+
   end
 end
