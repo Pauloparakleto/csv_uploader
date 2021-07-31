e7821a8a687c4f333ea282f058a6bff7e18adafd
Pauloparakleto <alparakleto@gmail.com>
2021-07-31 14:23:27 -0300

    chore(inventory_spec.rb): rubocop autocorrect

--- spec/business/csv_extractor_spec.rb
+++ spec/business/csv_extractor_spec.rb
@@ -18,6 +18,26 @@ RSpec.describe CsvExtractor do
         expect(result).to eq(true)
       end
     end
+
+    context "when build" do
+      it "first instance of inventory" do
+        path = "spec/support/input_valid.csv"
+        result = described_class.new(path: path).build
+        expect(result[0].class.name).to eq("Inventory")
+      end
+
+      it "inventory name" do
+        path = "spec/support/input_valid.csv"
+        result = described_class.new(path: path).build
+        expect(result[0].name).to eq("MyString")
+      end
+
+      it "second instance of inventory" do
+        path = "spec/support/input_valid.csv"
+        result = described_class.new(path: path).build
+        expect(result[1].class.name).to eq("Inventory")
+      end
+    end
   end
 
   describe "Invalid" do
