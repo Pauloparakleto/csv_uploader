f072fa76dea11f7555adc75a89a1828827c17f50
Pauloparakleto <alparakleto@gmail.com>
2021-07-30 21:22:38 -0300

    refactor(csv_spec.rb): fix described class offense

--- spec/business/csv_spec.rb
+++ spec/business/csv_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe CSV do
   describe "Valid" do
     context "when extract " do
       it "file path" do
-        csv = CSV.read("spec/support/input_valid.csv")
+        csv = described_class.read("spec/support/input_valid.csv")
         expect(csv).to be_truthy
       end
     end
