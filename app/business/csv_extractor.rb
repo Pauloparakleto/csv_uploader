519d731c16df2ef1d8e5ca17aa96ed740e12432d
Pauloparakleto <alparakleto@gmail.com>
2021-07-31 14:43:03 -0300

    refactor(csv_extractor.rb): avoid method too long in build

--- app/business/csv_extractor.rb
+++ app/business/csv_extractor.rb
@@ -4,33 +4,29 @@ class CsvExtractor
   end
 
   def valid?
-    data = CSV.read(@path)
+    data = csv_data
     result = []
     data.each { |i| result << false if i.any? nil }
     result.empty?
   end
 
+  def csv_data
+    CSV.read(@path)
+  end
+
   def build
-    data = CSV.read(@path)
     array = []
-    data.each do
+    csv_data.each do
       array << Inventory.create(attributes)
     end
     array
   end
 
   def attributes
-    data = CSV.read(@path)
+    csv_data
     {
-      name: "MyString",
-      value: "MyString",
-      quantity: "MyString",
-      manufacturer: "MyString",
-      model: "MyString",
-      color: "MyString",
-      carrier_plan_type: "MyString",
-      price: 999.98,
+      name: "MyString", value: "MyString", quantity: "MyString", manufacturer: "MyString", model: "MyString",
+      color: "MyString", carrier_plan_type: "MyString", price: 999.98
     }
-
   end
 end
