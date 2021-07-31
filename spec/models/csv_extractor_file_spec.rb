552d887a1702c25025eef314f5ad98d42a1a3815
Pauloparakleto <alparakleto@gmail.com>
2021-07-31 14:46:30 -0300

    refactor(csv_extractor_file_spec.rb): remove Bot way avoiding empty block definition on factory

--- spec/models/csv_extractor_file_spec.rb
+++ spec/models/csv_extractor_file_spec.rb
@@ -1,8 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe CsvExtractorFile, type: :model do
   it "has a blob file" do
-    csv_extractor_file = FactoryBot.create(:csv_extractor_file)
+    csv_extractor_file = described_class.create
     csv_extractor_file.csv_file.attach(io: File.open("spec/support/input_valid.csv"), filename: "input_valid.csv")
     expect(csv_extractor_file.csv_file).to be_truthy
   end
