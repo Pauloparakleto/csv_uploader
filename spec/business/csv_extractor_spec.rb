2000155661f17869b64a39bc54260aadcbe24ee1
Pauloparakleto <alparakleto@gmail.com>
2021-07-31 17:01:34 -0300

    test(csv_extractor_spec.rb): add check attributes details to the first inventory only

--- spec/business/csv_extractor_spec.rb
+++ spec/business/csv_extractor_spec.rb
@@ -37,6 +37,62 @@ RSpec.describe CsvExtractor do
         expect(result[1].class.name).to eq("Inventory")
       end
     end
+
+    context "when extract attributes" do
+      it "manufacturer" do
+        path = "spec/support/input_valid.csv"
+        data = CSV.read("spec/support/input_valid.csv")
+        manufacturer = data[1][0]
+        result = described_class.new(path: path).build
+
+        expect(result[0].manufacturer).to eq(manufacturer)
+      end
+
+      it "model" do
+        path = "spec/support/input_valid.csv"
+        data = CSV.read("spec/support/input_valid.csv")
+        model = data[1][1]
+        result = described_class.new(path: path).build
+
+        expect(result[0].model).to eq(model)
+      end
+
+      it "color" do
+        path = "spec/support/input_valid.csv"
+        data = CSV.read("spec/support/input_valid.csv")
+        color = data[1][2]
+        result = described_class.new(path: path).build
+
+        expect(result[0].color).to eq(color)
+      end
+
+      it "carrier_plan_type" do
+        path = "spec/support/input_valid.csv"
+        data = CSV.read("spec/support/input_valid.csv")
+        carrier_plan_type = data[1][3]
+        result = described_class.new(path: path).build
+
+        expect(result[0].carrier_plan_type).to eq(carrier_plan_type)
+      end
+
+      it "quantity" do
+        path = "spec/support/input_valid.csv"
+        data = CSV.read("spec/support/input_valid.csv")
+        quantity = data[1][4].to_i
+        result = described_class.new(path: path).build
+
+        expect(result[0].quantity).to eq(quantity)
+      end
+
+      it "price" do
+        path = "spec/support/input_valid.csv"
+        data = CSV.read("spec/support/input_valid.csv")
+        price = data[1][5].to_i
+        result = described_class.new(path: path).build
+
+        expect(result[0].price).to eq(price)
+      end
+    end
   end
 
   describe "Invalid" do
