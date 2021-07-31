4e9886046822e924ac77cc47977e5c2d2d9488c8
Pauloparakleto <alparakleto@gmail.com>
2021-07-31 18:24:47 -0300

    feat(csv_extractor.rb): add index to attributes

--- app/business/csv_extractor.rb
+++ app/business/csv_extractor.rb
@@ -16,17 +16,20 @@ class CsvExtractor
 
   def build
     array = []
-    csv_data.each do
-      array << Inventory.create(attributes)
+    index = 1
+    csv_data.each do |table|
+      next if table[0] == "manufacturer"
+      array << Inventory.create(attributes(index))
+      index += 1
     end
     array
   end
 
-  def attributes
+  def attributes(index)
     csv_data
     {
       name: "MyString", value: "MyString", manufacturer: csv_data[1][0], model: csv_data[1][1],
-      color: csv_data[1][2], carrier_plan_type: csv_data[1][3], quantity: csv_data[1][4], price: csv_data[1][5].to_i
+      color: csv_data[1][2], carrier_plan_type: csv_data[index][3], quantity: csv_data[1][4], price: csv_data[index][5].to_i
     }
   end
 end
