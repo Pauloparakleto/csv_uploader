046b2400bca10c74ec31641c3c0fd2740b9d9f20
Pauloparakleto <alparakleto@gmail.com>
2021-08-01 19:46:03 -0300

    test(inventory_spec.rb): add invalid csv request scenario

--- spec/integration/inventories_spec.rb
+++ spec/integration/inventories_spec.rb
@@ -13,4 +13,24 @@ RSpec.describe "Inventories", type: :request do
     post api_v1_inventories_path, params: { csv_file: fixture_file_upload("input_valid.csv") }, headers: headers
     expect(response).to have_http_status(:created)
   end
+
+  describe "Invalid Csv" do
+    it "create" do
+      headers = { "ACCEPT" => "application/html" }
+      post api_v1_inventories_path, params: { csv_file: fixture_file_upload("input_invalid.csv") }, headers: headers
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it "nil csv create" do
+      headers = { "ACCEPT" => "application/html" }
+      post api_v1_inventories_path, params: { csv_file: nil }, headers: headers
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it "empty csv create" do
+      headers = { "ACCEPT" => "application/html" }
+      post api_v1_inventories_path, params: { csv_file: "" }, headers: headers
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
 end
