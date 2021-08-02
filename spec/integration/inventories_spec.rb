c405c204fad79e29a3124abf90cc92a675a4b371
Pauloparakleto <alparakleto@gmail.com>
2021-08-02 09:28:44 -0300

    test(inventories_spec.rb): add update context valid

--- spec/integration/inventories_spec.rb
+++ spec/integration/inventories_spec.rb
@@ -1,36 +1,53 @@
 require "rails_helper"
 
 RSpec.describe "Inventories", type: :request do
-  it "index" do
-    FactoryBot.create_list(:inventory, 3)
-    headers = { "ACCEPT" => "application/html" }
-    get "/api/v1/inventories", headers: headers
-    expect(response).to have_http_status(:ok)
-  end
+  describe "Status Response" do
+    context "when create" do
 
-  it "create by csv" do
-    headers = { "ACCEPT" => "application/html" }
-    post api_v1_inventories_path, params: { csv_file: fixture_file_upload("input_valid.csv") }, headers: headers
-    expect(response).to have_http_status(:created)
-  end
+      it "index" do
+        FactoryBot.create_list(:inventory, 3)
+        headers = { "ACCEPT" => "application/html" }
+        get "/api/v1/inventories", headers: headers
+        expect(response).to have_http_status(:ok)
+      end
 
-  describe "Invalid Csv" do
-    it "create" do
-      headers = { "ACCEPT" => "application/html" }
-      post api_v1_inventories_path, params: { csv_file: fixture_file_upload("input_invalid.csv") }, headers: headers
-      expect(response).to have_http_status(:unprocessable_entity)
+      it "create by csv" do
+        headers = { "ACCEPT" => "application/json" }
+        post api_v1_inventories_path, params: { csv_file: fixture_file_upload("input_valid.csv") }, headers: headers
+        expect(response).to have_http_status(:created)
+      end
     end
 
-    it "nil csv create" do
-      headers = { "ACCEPT" => "application/html" }
-      post api_v1_inventories_path, params: { csv_file: nil }, headers: headers
-      expect(response).to have_http_status(:unprocessable_entity)
+    context "when update" do
+      before do
+        post api_v1_inventories_path, params: { csv_file: fixture_file_upload("input_valid.csv") }, headers: headers
+      end
+      
+      it "by csv" do
+        headers = { "ACCEPT" => "application/json" }
+        post update_csv_api_v1_inventories_url, params: { csv_file: fixture_file_upload("input_valid_update.csv") }, headers: headers
+        expect(response).to have_http_status(:ok)
+      end
     end
 
-    it "empty csv create" do
-      headers = { "ACCEPT" => "application/html" }
-      post api_v1_inventories_path, params: { csv_file: "" }, headers: headers
-      expect(response).to have_http_status(:unprocessable_entity)
+    describe "Invalid Csv" do
+      it "create" do
+        headers = { "ACCEPT" => "application/html" }
+        post api_v1_inventories_path, params: { csv_file: fixture_file_upload("input_invalid.csv") }, headers: headers
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it "nil csv create" do
+        headers = { "ACCEPT" => "application/json" }
+        post api_v1_inventories_path, params: { csv_file: nil }, headers: headers
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it "empty csv create" do
+        headers = { "ACCEPT" => "application/html" }
+        post api_v1_inventories_path, params: { csv_file: "" }, headers: headers
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
     end
   end
 end
