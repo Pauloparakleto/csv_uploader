94a34987d0339b92198ee91f87156fb8c188495d
Pauloparakleto <alparakleto@gmail.com>
2021-08-02 12:35:35 -0300

    chore(factories/inventories.rb): add more sample

--- spec/factories/inventories.rb
+++ spec/factories/inventories.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :inventory do
     name { "MyString" }
     value { "MyString" }
-    quantity { "MyString" }
-    manufacturer { "MyString" }
-    model { "MyString" }
-    color { "MyString" }
-    carrier_plan_type { "MyString" }
+    quantity { [20, 20, 20, 20, 10, 5, 5].sample }
+    manufacturer { %w[Motorola Samsung Apple].sample }
+    model { ["Moto G5 16GB" ].sample }
+    color { %w[Preto Cinza].sample }
+    carrier_plan_type { %w[Pré Pós].sample }
     price { 999.98 }
   end
 end
