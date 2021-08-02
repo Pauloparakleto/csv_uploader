8a1b84bb6584a4186a2f2fad5939e69ff335bd3a
Pauloparakleto <alparakleto@gmail.com>
2021-08-02 12:37:00 -0300

    chore(factories/inventories.rb): fix offense space inside array

--- spec/factories/inventories.rb
+++ spec/factories/inventories.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     value { "MyString" }
     quantity { [20, 20, 20, 20, 10, 5, 5].sample }
     manufacturer { %w[Motorola Samsung Apple].sample }
-    model { ["Moto G5 16GB" ].sample }
+    model { ["Moto G5 16GB"].sample }
     color { %w[Preto Cinza].sample }
     carrier_plan_type { %w[Pré Pós].sample }
     price { 999.98 }
