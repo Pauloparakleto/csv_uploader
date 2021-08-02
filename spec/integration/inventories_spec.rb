e4dc915764dd43a9830713bc09221fdd68418ec6
Pauloparakleto <alparakleto@gmail.com>
2021-08-02 17:10:53 -0300

    test(inventories_spec.rb): add csv extract request

--- spec/integration/inventories_spec.rb
+++ spec/integration/inventories_spec.rb
@@ -13,6 +13,29 @@ RSpec.describe "Inventories", type: :request do
     end
   end
 
+  describe "Extract CSV Action" do
+
+    context "when update" do
+      before do
+        headers = { "ACCEPT" => "application/html" }
+        post extract_csv_api_v1_inventories_path, params: { csv_file: fixture_file_upload("input_valid.csv") }, headers: headers
+      end
+
+      it "update by csv" do
+        headers = { "ACCEPT" => "application/html" }
+        post extract_csv_api_v1_inventories_path, params: { csv_file: fixture_file_upload("input_valid_update.csv") }, headers: headers
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    it "create by csv" do
+      headers = { "ACCEPT" => "application/html" }
+      post extract_csv_api_v1_inventories_path, params: { csv_file: fixture_file_upload("input_valid.csv") }, headers: headers
+      expect(response).to have_http_status(:created)
+    end
+
+  end
+
   describe "Status Response" do
     context "when create" do
       it "index" do
@@ -43,21 +66,21 @@ RSpec.describe "Inventories", type: :request do
       it "by csv" do
         headers = { "ACCEPT" => "application/html" }
         post update_csv_api_v1_inventories_url, params: { csv_file: fixture_file_upload("input_valid_update.csv") },
-                                                headers: headers
+             headers: headers
         expect(response).to have_http_status(:ok)
       end
 
       it "by csv invalid message body" do
         headers = { "ACCEPT" => "application/html" }
         post update_csv_api_v1_inventories_url, params: { csv_file: fixture_file_upload("input_invalid.csv") },
-                                                headers: headers
+             headers: headers
         expect(response.body).to eq("O Arquivo CSV possui campos em branco.")
       end
 
       it "by csv invalid status" do
         headers = { "ACCEPT" => "application/html" }
         post update_csv_api_v1_inventories_url, params: { csv_file: fixture_file_upload("input_invalid.csv") },
-                                                headers: headers
+             headers: headers
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
